e41ba2fbf86a6552a5aeb160bf665e3df0642b54
Wanderson Sousa <wanderson.sousa@basis.com.br>
2023-06-02 21:21:56 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaCompra.Domain/SolicitacaoCompraAggregate/CondicaoPagamento.cs
@@ -0,0 +1,24 @@
+﻿using SistemaCompra.Domain.Core;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
+{
+    public class CondicaoPagamento
+    {
+        private IList<int> _valoresPossiveis = new List<int>() { 0, 30, 60, 90 };
+        public int Valor { get; private set; }
+
+        private CondicaoPagamento(){}
+
+        public CondicaoPagamento(int condicao, decimal totalGeral)
+        {
+            if (totalGeral > 5000)
+                condicao = 30;
+
+            if (!_valoresPossiveis.Contains(condicao)) throw new BusinessRuleException("Condição de pagamento deve ser " +_valoresPossiveis.ToString());
+
+            Valor = condicao;
+        }
+    }
+}
---
+++ SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
@@ -0,0 +1,45 @@
+﻿using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.Core.Model;
+using SistemaCompra.Domain.ProdutoAggregate;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
+{
+    public class SolicitacaoCompra : Entity
+    {
+        public UsuarioSolicitante UsuarioSolicitante { get; private set; }
+        public NomeFornecedor NomeFornecedor { get; private set; }
+        public IList<Item> Itens { get; private set; }
+        public DateTime Data { get; private set; }
+        public Money TotalGeral { get; private set; }
+        public Situacao Situacao { get; private set; }
+        public CondicaoPagamento CondicaoPagamento { get; private set; }
+        private SolicitacaoCompra() { }
+
+        public SolicitacaoCompra(string usuarioSolicitante, string nomeFornecedor, decimal totalGeral = 0, int condicaoPagamento = 0)
+        {
+            Id = Guid.NewGuid();
+            UsuarioSolicitante = new UsuarioSolicitante(usuarioSolicitante);
+            NomeFornecedor = new NomeFornecedor(nomeFornecedor);
+            Data = DateTime.Now;
+            Situacao = Situacao.Solicitado;
+            TotalGeral = new Money(totalGeral);
+            CondicaoPagamento = new CondicaoPagamento(condicaoPagamento, totalGeral);
+        }
+
+        public void AdicionarItem(Produto produto, int qtde)
+        {
+            Itens.Add(new Item(produto, qtde));
+        }
+
+        public void RegistrarCompra(IEnumerable<Item> itens)
+        {
+            if (itens.Count() == 0) 
+                throw new BusinessRuleException("A solicitação de compra deve possuir itens!");
+            AddEvent(new CompraRegistradaEvent(Id, itens, TotalGeral.Value));
+        }
+    }
+}
